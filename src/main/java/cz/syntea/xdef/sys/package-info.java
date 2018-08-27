c751976ffe6468c2513a1b26a873550407ea9463
Vladimír Šišma <vladimir.sisma@syntea.cz>
2018-08-27 08:53:34 +0200

    standardization of unit-tests: java language version splitting: main in 1.6, tests in 1.8

--- src/main/java/cz/syntea/xdef/sys/package-info.java
+++ src/main/java/cz/syntea/xdef/sys/package-info.java
@@ -114,19 +114,5 @@
  * {@link cz.syntea.xdef.sys.SObjectWriter} and you car read them by
  * {@link cz.syntea.xdef.sys.SObjectReader}.
  * <p>For generation of unique IDs you can use for multi VM</p>
- *
- * <H2><code>STester</code></H2>
- *
- * <UL>
- *
- * <li>
- * STester is the abstract class which provides tools for testing.
- * This class requires following structure of directories:
- * <p><code>build/classes src/javaSource_dataDir[/dataDir] or
- * test/javaSource_dataDir[/dataDir]</code>
- * <p>See {@link test.util.STester}.</p>
- * </li>
- *
- * </UL>
  */
 package cz.syntea.xdef.sys;
---
+++ src/test/java/test/util/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * <H2><code>STester</code></H2>
+ *
+ * <UL>
+ *
+ * <li>
+ * STester is the abstract class which provides tools for testing.
+ * This class requires following structure of directories:
+ * <p><code>build/classes src/javaSource_dataDir[/dataDir] or
+ * test/javaSource_dataDir[/dataDir]</code>
+ * <p>See {@link test.util.STester}.</p>
+ * </li>
+ *
+ * </UL>
+ */
+package test.util;
