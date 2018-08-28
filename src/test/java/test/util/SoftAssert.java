2b28d945dbab185cc0de302b95bae8a124f477f3
Vladimír Šišma <vladimir.sisma@syntea.cz>
2018-08-28 18:46:30 +0200

    standardization of unit-tests: SoftAssert: print exception during assertion-fail

--- src/test/java/test/util/SoftAssert.java
+++ src/test/java/test/util/SoftAssert.java
@@ -2,6 +2,8 @@ package test.util;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.asserts.IAssert;
 import org.testng.collections.Maps;
 
@@ -23,6 +25,23 @@ public class SoftAssert extends Assertion {
 		} catch (AssertionError ex) {
 			onAssertFailure(a, ex);
 			m_errors.put(ex, a);
+			
+			String   exST = TestUtil.exceptionStackTrace(ex);
+			String[] exAr = exST.split("\n");
+			String   matchString = "at " + getClass().getName() + ".doAssert";
+			int j = -1;
+			for (int i = 0; i < exAr.length; i++) {
+				if (exAr[i].contains(matchString)) {
+					j = i;
+					break;
+				}
+			}
+			
+			if (j >= 0) {
+				logger.error(ex.getMessage() + "\n" + exAr[j + 2]);
+			} else {
+				logger.error(ex.getMessage() + "\n" + exST);
+			}
 		} finally {
 			onAfterAssert(a);
 		}
@@ -46,5 +65,8 @@ public class SoftAssert extends Assertion {
 			throw new AssertionError(sb.toString());
 		}
 	}
+
+	/** logger */
+	private static final Logger logger = LoggerFactory.getLogger(SoftAssert.class);
 
 }
