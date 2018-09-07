8db44e721682db52016beed0d4d528ba0b4775bd
Vladimír Šišma <vladimir.sisma@syntea.cz>
2018-09-07 02:33:21 +0200

    adopt minimal testng, add pominfo.properties, better pom.xml

--- src/test/java/test/TestAll.java
+++ src/test/java/test/TestAll.java
@@ -12,12 +12,14 @@
  */
 package test;
 
-import org.testng.ITestResult;
-import org.testng.TestListenerAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.Assert;
 import org.testng.TestNG;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
+
 import test.utils.XDTester;
 
 /** Execute all tests fast.
@@ -34,62 +36,42 @@ public class TestAll {
     /** run TestAll in test.common */
     @Test
     public static void testCommon() {
-        final Assertion  a     = new Assertion();
-        final String[]   args0 = {};
-        a.assertEquals(test.common.TestAll.runTests(args0), 0);
+        Assert.assertEquals(test.common.TestAll.runTests(new String[0]), 0);
     }
         
     /** run TestAll in test.xdef */
     @Test(dependsOnMethods = {"testCommon"})
     public static void testXdef() {
-        final Assertion  a     = new Assertion();
-        final String[]   args0 = {};        
-        a.assertEquals(test.xdef.TestAll.runTests(args0), 0);
+        Assert.assertEquals(test.xdef.TestAll.runTests(new String[0]), 0);
     }
     
     /** run TestAll in test.xdutil */
     @Test(dependsOnMethods = {"testXdef"})
     public static void testXDUtils() {
-        final Assertion  a     = new Assertion();
-        final String[]   args0 = {};
-        a.assertEquals(test.xdutils.TestAll.runTests(args0), 0);
+        Assert.assertEquals(test.xdutils.TestAll.runTests(new String[0]), 0);
     }
 
     
     
     /**Run tests with TestNG */
     public static void mainTestNG() {
-        TestNG              testNG = new TestNG();
-        TestListenerAdapter tla    = new TestListenerAdapter();
-        testNG.setTestClasses(new Class<?>[] {TestAll.class});
-        testNG.setOutputDirectory("run/output/test-ng");
-        testNG.addListener(tla);
-        testNG.run();
-
-        for (ITestResult result : tla.getFailedTests()) {
-            String id = result.getTestClass().getName() 
-                + "." + result.getName();
-            Throwable ex = result.getThrowable();
-            if (ex != null) {
-                System.err.println(id + ":");
-                ex.printStackTrace();
-            } else {
-                System.err.println(id + ": failure without exception!");
-            }
-        }
+		List<String> suiteList = new ArrayList<String>();
+		suiteList.add("src/test/resources/testng.xml");
+		
+		TestNG testNG = new TestNG();
+		testNG.setTestSuites(suiteList);
+		//testNG.setTestClasses(new Class<?>[] {TestAll.class});
+		testNG.setOutputDirectory("target/test-output/report");
+		testNG.run();
     }
 
     /** Run all test directly */
     public static void mainTest() {
         beforeTests();
         
-        try {
-            testCommon();
-            testXdef();
-            testXDUtils();
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
+        testCommon();
+        testXdef();
+        testXDUtils();
     }
 
  	
