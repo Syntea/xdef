92d2f79f2ce159f8d078a7415b02c7fab0930fc2
Václav Trojan <trojan@syntea.cz>
2018-12-20 18:03:43 +0100

    .

---
+++ src/test/java/builtools/Nop.java
@@ -0,0 +1,16 @@
+package builtools;
+
+/**
+ *
+ * @author Vaclav Trojan
+ */
+public class Nop {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
