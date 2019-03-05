64f78ad20d7c7aa91d0e73e5e8c3f7b370bc4e0d
Václav Trojan <trojan@syntea.cz>
2019-03-05 18:21:52 +0100

    . fixed classpath separator for different platforms

--- src/test/java/test/utils/XDTester.java
+++ src/test/java/test/utils/XDTester.java
@@ -1113,8 +1113,8 @@ public abstract class XDTester extends STester {
 		classDir = classDir.substring(0, classDir.indexOf(className));
 		// prepare parameters
 		ArrayList<String> ar = new ArrayList<String>();
-		ar.add("-classpath"); ar.add(classpath +
-			System.getProperty("path.separator") + classDir); // classpath
+		ar.add("-classpath"); ar.add(classpath
+			+ System.getProperty("path.separator") + classDir); // classpath
 		ar.add("-d"); ar.add(classDir); // where to write compiled classes
 		ar.add("-source"); ar.add("1.6"); // sources are in version 1.6
 		ar.add("-target"); ar.add("1.6"); // generate classes in version 1.6
@@ -1128,9 +1128,8 @@ public abstract class XDTester extends STester {
 		ByteArrayOutputStream err = new ByteArrayOutputStream();
 		// compile sources
 		if (compiler.run(null, out, err, ar.toArray(new String[0])) != 0) {
-			throw new RuntimeException("Java compilation failed:\n" 
+			throw new RuntimeException("Java compilation failed:\n"
 				+ new String(err.toByteArray()));
-			
 		}
 	}
 
