0165364b81291a8eb128177c75932379daf18603
Václav Trojan <trojan@syntea.cz>
2019-09-30 19:35:41 +0200

    .

--- src/test/java/test/xdef/TestDebugGUI1.java
+++ src/test/java/test/xdef/TestDebugGUI1.java
@@ -22,10 +22,10 @@ public class TestDebugGUI1 {
 			"oxygeneditor.OxygenEditor");
         props.setProperty(XDConstants.XDPROPERTY_DISPLAY,
 			XDConstants.XDPROPERTYVALUE_DISPLAY_TRUE);
-//        File xd0 = new File("src/testexternaleditor/test1.xdef");
-//        File xd1 = new File("src/testexternaleditor/test2.xml");
-//        Object[] srcs = new Object[]{xd0};
-		Object[] srcs = null;
+        File xd0 = new File("src/testexternaleditor/test1.xdef");
+        File xd1 = new File("src/testexternaleditor/test2.xml");
+        Object[] srcs = new Object[]{xd0};
+//		Object[] srcs = null;
         try {
             XDPool xp = XDFactory.compileXD(props, srcs); 
             System.out.println("Ok.");
