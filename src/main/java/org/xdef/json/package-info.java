aba8ebd671be07439c22213a16cadf2b1ca6b7c0
V�clav Trojan <trojan@syntea.cz>
2020-01-07 00:00:54 +0100

    .

--- src/main/java/org/xdef/json/package-info.java
+++ src/main/java/org/xdef/json/package-info.java
@@ -2,9 +2,9 @@
  * <p>
  * Provides tools for using JSON in X-definitions.
  * </p>
- * <p>{@link cz.syntea.xdef.json.JsonUtil}</p>
- * <p>{@link cz.syntea.xdef.json.JsonToXml}</p>
- * <p>{@link cz.syntea.xdef.json.XmlToJson}</p>
- * <p>{@link cz.syntea.xdef.json.JNull}</p>
+ * <p>{@link org.xdef.json.JsonUtil}</p>
+ * <p>{@link org.xdef.json.JsonToXml}</p>
+ * <p>{@link org.xdef.json.XmlToJson}</p>
+ * <p>{@link org.xdef.json.JNull}</p>
  */
 package org.xdef.json;
