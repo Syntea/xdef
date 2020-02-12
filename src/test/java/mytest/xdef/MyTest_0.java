385863ee0eb2d0ed3e35e6a92cc5e2d03c771c20
V�clav Trojan <trojan@syntea.cz>
2020-02-12 01:07:54 +0100

    .

--- src/test/java/mytest/xdef/MyTest_0.java
+++ src/test/java/mytest/xdef/MyTest_0.java
@@ -19,7 +19,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.List;
 import java.util.Properties;
 import java.util.Stack;
 import org.xdef.XDContainer;
