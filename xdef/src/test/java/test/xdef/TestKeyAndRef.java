73e3890cfa1cbdabe08020cee4627bfd7366346f
Vßclav Trojan <trojan@syntea.cz>
2020-07-02 13:40:45 +0200

    .

--- xdef/src/test/java/test/xdef/TestKeyAndRef.java
+++ xdef/src/test/java/test/xdef/TestKeyAndRef.java
@@ -1168,8 +1168,8 @@ public final class TestKeyAndRef extends XDTester {
 			assertNoErrors(reporter);
 			assertEq("1,2,3,4,5,2,5,1,4,3,", strw.toString());
 		} catch (Exception ex) {fail(ex);}
-		try { // test of uniqueSetKey
-			xdef =
+		try { // test of uniqueSetKey and bindSet
+			xdef = // uniqueSetKey
 "<xd:def xmlns:xd='" + XDConstants.XDEF40_NS_URI + "' root='CodeBook'>\n"+
 " <xd:declaration scope=\"local\">\n" +
 "     int  AttrCount;\n" +
@@ -1185,55 +1185,89 @@ public final class TestKeyAndRef extends XDTester {
 "     boolean chkNode() {return nodeSet.Node.CHKID() AAND attrSet.Node();}\n"+
 " </xd:declaration>\n" +
 "\n" +
-" <CodeBook Name=\"name()\"\n" +
-"           Version=\"version()\"\n" +
-"           Description=\"? description()\">\n" +
-"   <Def xd:script=\"0..1;\"> <!-- Code list definitions -->\n" +
-"     <Node  xd:script=\"1..;\" Name=\"idNode();\">\n" +
-"       <Attr xd:script=\"1..;\" Name=\"attrSet.Attr.ID()\"/>\n" +
-"       <Node xd:script=\"0..; ref CodeBook/Def/Node\"/>\n" +
-"     </Node>\n" +
-"   </Def>\n" +
-"   <Values xd:script=\"0..1;\"> <!-- Code list values  -->\n" +
-"     <Node xd:script=\"var uniqueSetKey x; 1..;\"\n" +
-"         Name=\"chkNode();\">\n" +
-"       <xd:sequence xd:script=\"1..\">\n" +
-"         <Row xd:script=\"1..;\">\n" +
-"           <Attr xd:script=\"1..;\" Name=\"attrSet.Attr.CHKID()\">\n" +
-"             attrValue()\n" +
-"           </Attr>\n" +
-"         </Row>\n" +
-"         <Node xd:script=\"0..; ref CodeBook/Values/Node;\n"+
-"                  init x=attrSet.getActualKey();finally x.resetKey();\"/>\n" +
-"       </xd:sequence>\n" +
-"     </Node>\n" +
-"   </Values>\n" +
-" </CodeBook>\n" +
+"<CodeBook>\n" +
+"  <Def xd:script=\"0..1;\"> <!-- Code list definitions -->\n" +
+"    <Node  xd:script=\"1..;\" Name=\"idNode();\">\n" +
+"      <Attr xd:script=\"1..;\" Name=\"attrSet.Attr.ID()\"/>\n" +
+"      <Node xd:script=\"0..; ref CodeBook/Def/Node\"/>\n" +
+"    </Node>\n" +
+"  </Def>\n" +
+"  <Node xd:script=\"var uniqueSetKey x=attrSet.getActualKey(); 0..*;\"\n" +
+"        Name=\"chkNode();\">\n" +
+"    <xd:sequence xd:script=\"1..*\">\n" +
+"      <Row xd:script=\"1..*;\">\n" +
+"        <Attr xd:script=\"1..*;\" Name=\"attrSet.Attr.CHKID()\">\n" +
+"          attrValue()\n" +
+"        </Attr>\n" +
+"      </Row>\n" +
+"      <Node xd:script=\"0..*; ref CodeBook/Node; finally x.resetKey();\"/>\n"+
+"    </xd:sequence>\n" +
+"  </Node>\n" +
+"</CodeBook>\n" +
 "</xd:def>";
 			xp = compile(xdef);
 			xml =
-"<CodeBook Name=\"Tabulka\" Version=\"1\">\n" +
+"<CodeBook>\n" +
 "  <Def>\n" +
 "    <Node Name=\"Tab_V\">\n" +
 "      <Attr Name=\"A\"/><Attr Name=\"B\"/>\n" +
 "      <Node Name=\"Tab_m\"><Attr Name=\"c\"/><Attr Name=\"d\"/></Node>\n" +
 "    </Node>\n" +
 "  </Def>\n" +
-"  <Values>\n" +
-"    <Node Name=\"Tab_V\">\n" +
-"      <Row><Attr Name=\"A\">A1</Attr><Attr Name=\"B\">B1</Attr></Row>\n" +
-"      <Node Name=\"Tab_m\">\n" +
-"        <Row><Attr Name=\"c\">1c1</Attr><Attr Name=\"d\">1d1</Attr></Row>\n" +
-"        <Row><Attr Name=\"c\">1c2</Attr><Attr Name=\"d\">1d2</Attr></Row>\n" +
-"      </Node>\n" +
-"      <Row> <Attr Name=\"A\">A2</Attr><Attr Name=\"B\">B2</Attr></Row>\n" +
-"      <Node Name=\"Tab_m\">\n" +
-"        <Row><Attr Name=\"c\">2c1</Attr><Attr Name=\"d\">2d1</Attr></Row>\n" +
-"        <Row><Attr Name=\"c\">2c2</Attr><Attr Name=\"d\">2d2</Attr></Row>\n" +
-"      </Node>\n" +
+"  <Node Name=\"Tab_V\">\n" +
+"    <Row><Attr Name=\"A\">A1</Attr><Attr Name=\"B\">B1</Attr></Row>\n" +
+"    <Node Name=\"Tab_m\">\n" +
+"      <Row><Attr Name=\"c\">1c1</Attr><Attr Name=\"d\">1d1</Attr></Row>\n" +
+"      <Row><Attr Name=\"c\">1c2</Attr><Attr Name=\"d\">1d2</Attr></Row>\n" +
 "    </Node>\n" +
-"  </Values>\n" +
+"    <Row> <Attr Name=\"A\">A2</Attr><Attr Name=\"B\">B2</Attr></Row>\n" +
+"    <Node Name=\"Tab_m\">\n" +
+"      <Row><Attr Name=\"c\">2c1</Attr><Attr Name=\"d\">2d1</Attr></Row>\n" +
+"      <Row><Attr Name=\"c\">2c2</Attr><Attr Name=\"d\">2d2</Attr></Row>\n" +
+"    </Node>\n" +
+"  </Node>\n" +
 "</CodeBook>";
+			assertEq(xml, parse(xp, "", xml, reporter));
+			assertNoErrors(reporter);
+			xdef = // bindSet
+"<xd:def xmlns:xd='" + XDConstants.XDEF40_NS_URI + "' root='CodeBook'>\n"+
+"  <xd:declaration scope=\"local\">\n" +
+"    int  AttrCount;\n" +
+"    type attrValue    string(1,511);\n" +
+"    type description  string(1,511);\n" +
+"    type name         string(1,30);\n" +
+"    type version      string(1,20);\n" +
+"    type xdate        xdatetime('yyyy-MM-dd');\n" +
+"    uniqueSet nodes   {Node: name(); var int AttrCount};\n" +
+"    uniqueSet atts    {Node: name(); Attr: name()};\n" +
+"\n" +
+"    boolean idNode()  {return nodes.Node.ID() AAND atts.Node();}\n"+
+"    boolean chkNode() {return nodes.Node.CHKID() AAND atts.Node();}\n"+
+"  </xd:declaration>\n" +
+"\n" +
+"<CodeBook>\n" +
+"  <!-- Code list definitions -->\n" +
+"  <Def>\n" +
+"    <Node  xd:script=\"1..*;\" Name=\"idNode();\">\n" +
+"      <Attr xd:script=\"1..*;\" Name=\"atts.Attr.ID()\"/>\n" +
+"      <Node xd:script=\"0..*; ref CodeBook/Def/Node\"/>\n" +
+"    </Node>\n" +
+"  </Def>\n" +
+"  <!-- Code list values  -->\n" +
+"  <Node xd:script=\"0..*;\"\n" +
+"        Name=\"chkNode();\">\n" +
+"    <xd:sequence xd:script=\"1..*\">\n" +
+"      <Row xd:script=\"1..*;\">\n" +
+"        <Attr xd:script=\"1..*;\" Name=\"atts.Attr.CHKID()\">\n" +
+"          attrValue()\n" +
+"        </Attr>\n" +
+"      </Row>\n" +
+"      <Node xd:script=\"0..*; init bindSet(atts); ref CodeBook/Node;\"/>\n" +
+"    </xd:sequence>\n" +
+"  </Node>\n" +
+"</CodeBook>\n" +
+"</xd:def>";
+			xp = compile(xdef);
 			assertEq(xml, parse(xp, "", xml, reporter));
 			assertNoErrors(reporter);
 		} catch (Exception ex) {fail(ex);}
