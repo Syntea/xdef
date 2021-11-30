398378b6212cfd3a16eaaaf49703ee33771d1c61
Vßclav Trojan <trojan@syntea.cz>
2021-11-30 16:36:11 +0100

    .

--- xdef/src/test/java/test/xdef/TestLexicon.java
+++ xdef/src/test/java/test/xdef/TestLexicon.java
@@ -6,7 +6,6 @@ import org.xdef.XDDocument;
 import org.xdef.XDPool;
 import org.xdef.component.XComponent;
 import java.io.File;
-import java.lang.reflect.Method;
 import java.util.List;
 import org.w3c.dom.Element;
 import org.xdef.msg.SYS;
@@ -32,7 +31,7 @@ public final class TestLexicon extends XDTester {
 		XDDocument xd;
 		try {
 			xdef =
-"<xd:def xmlns:xd='" + _xdNS + "' root='Contract' name='kontrakt'>\n"+
+"<xd:def xmlns:xd='" + _xdNS + "' root='Contract' name='contract'>\n"+
 "\n"+
 "<Contract Number=\"num()\">\n"+
 "  <Client xd:script=\"+\"\n"+
@@ -60,61 +59,61 @@ public final class TestLexicon extends XDTester {
 "              create from('@GivenName') + ' ' + from('@LastName');\"/>\n"+
 "</Agreement>\n"+
 "<xd:component>\n"+
-"  %class test.xdef.component.L_Contract %link kontrakt#Contract;\n"+
+"  %class test.xdef.component.L_Contract %link contract#Contract;\n"+
 "</xd:component>\n"+
 "</xd:def>";
 			String lexicon1 =
 "<xd:lexicon xmlns:xd='" + _xdNS + "' language='eng'>\n"+
-"kontrakt#Contract =                         Contract\n"+
-"kontrakt#Contract/@Number =                 Number\n"+
-"kontrakt#Contract/Client =                  Client\n"+
-"kontrakt#Contract/Client/@Typ =             Typ\n"+
-"kontrakt#Contract/Client/@Name =            Name\n"+
-"kontrakt#Contract/Client/@ID =              ID\n"+
-"kontrakt#Contract/Client/@GivenName =       GivenName\n"+
-"kontrakt#Contract/Client/@LastName =        LastName\n"+
-"kontrakt#Contract/Client/@PersonalID =      PersonalID\n"+
-"kontrakt#Agreement =                        Agreement\n"+
-"kontrakt#Agreement/@Date =                  Date\n"+
-"kontrakt#Agreement/@Number =                Number\n"+
-"kontrakt#Agreement/Owner =                  Owner\n"+
-"kontrakt#Agreement/Owner/@ID =              ID\n"+
-"kontrakt#Agreement/Owner/@Name =            Name\n"+
-"kontrakt#Agreement/Holder =                 Holder\n"+
-"kontrakt#Agreement/Holder/@PID =            PID\n"+
-"kontrakt#Agreement/Holder/@GivenName =      GivenName\n"+
-"kontrakt#Agreement/Holder/@LastName =       LastName\n"+
-"kontrakt#Agreement/Mediator =               Mediator\n"+
-"kontrakt#Agreement/Mediator/@ID =           ID\n"+
-"kontrakt#Agreement/Mediator/@Name =         Name\n"+
+"contract#Contract =                         Contract\n"+
+"contract#Contract/@Number =                 Number\n"+
+"contract#Contract/Client =                  Client\n"+
+"contract#Contract/Client/@Typ =             Typ\n"+
+"contract#Contract/Client/@Name =            Name\n"+
+"contract#Contract/Client/@ID =              ID\n"+
+"contract#Contract/Client/@GivenName =       GivenName\n"+
+"contract#Contract/Client/@LastName =        LastName\n"+
+"contract#Contract/Client/@PersonalID =      PersonalID\n"+
+"contract#Agreement =                        Agreement\n"+
+"contract#Agreement/@Date =                  Date\n"+
+"contract#Agreement/@Number =                Number\n"+
+"contract#Agreement/Owner =                  Owner\n"+
+"contract#Agreement/Owner/@ID =              ID\n"+
+"contract#Agreement/Owner/@Name =            Name\n"+
+"contract#Agreement/Holder =                 Holder\n"+
+"contract#Agreement/Holder/@PID =            PID\n"+
+"contract#Agreement/Holder/@GivenName =      GivenName\n"+
+"contract#Agreement/Holder/@LastName =       LastName\n"+
+"contract#Agreement/Mediator =               Mediator\n"+
+"contract#Agreement/Mediator/@ID =           ID\n"+
+"contract#Agreement/Mediator/@Name =         Name\n"+
 "</xd:lexicon>";
 			String lexicon2 =
 "<xd:lexicon xmlns:xd='" + _xdNS + "' language='ces'>\n"+
-"kontrakt#Contract =                         Smlouva\n"+
-"kontrakt#Contract/@Number =                 Číslo\n"+
-"kontrakt#Contract/Client =                  Klient\n"+
-"kontrakt#Contract/Client/@Typ =             Role\n"+
-"kontrakt#Contract/Client/@Name =            Název\n"+
-"kontrakt#Contract/Client/@ID =              IČO\n"+
-"kontrakt#Contract/Client/@GivenName =       Jméno\n"+
-"kontrakt#Contract/Client/@LastName =        Příjmení\n"+
-"kontrakt#Contract/Client/@PersonalID =      RodnéČíslo\n"+
-"kontrakt#Agreement =                        Dohoda\n"+
-"kontrakt#Agreement/@Date =                  Datum\n"+
-"kontrakt#Agreement/@Number =                Číslo\n"+
-"kontrakt#Agreement/Owner =                  Vlastník\n"+
-"kontrakt#Agreement/Owner/@ID =              IČO\n"+
-"kontrakt#Agreement/Owner/@Name =            Název\n"+
-"kontrakt#Agreement/Holder =                 Držitel\n"+
-"kontrakt#Agreement/Holder/@PID =            RČ\n"+
-"kontrakt#Agreement/Holder/@GivenName =      Jméno\n"+
-"kontrakt#Agreement/Holder/@LastName =       Příjmení\n"+
-"kontrakt#Agreement/Mediator =               Prostředník\n"+
-"kontrakt#Agreement/Mediator/@ID =           IČO\n"+
-"kontrakt#Agreement/Mediator/@Name =         Název\n"+
+"contract#Contract =                         Smlouva\n"+
+"contract#Contract/@Number =                 Číslo\n"+
+"contract#Contract/Client =                  Klient\n"+
+"contract#Contract/Client/@Typ =             Role\n"+
+"contract#Contract/Client/@Name =            Název\n"+
+"contract#Contract/Client/@ID =              IČO\n"+
+"contract#Contract/Client/@GivenName =       Jméno\n"+
+"contract#Contract/Client/@LastName =        Příjmení\n"+
+"contract#Contract/Client/@PersonalID =      RodnéČíslo\n"+
+"contract#Agreement =                        Dohoda\n"+
+"contract#Agreement/@Date =                  Datum\n"+
+"contract#Agreement/@Number =                Číslo\n"+
+"contract#Agreement/Owner =                  Vlastník\n"+
+"contract#Agreement/Owner/@ID =              IČO\n"+
+"contract#Agreement/Owner/@Name =            Název\n"+
+"contract#Agreement/Holder =                 Držitel\n"+
+"contract#Agreement/Holder/@PID =            RČ\n"+
+"contract#Agreement/Holder/@GivenName =      Jméno\n"+
+"contract#Agreement/Holder/@LastName =       Příjmení\n"+
+"contract#Agreement/Mediator =               Prostředník\n"+
+"contract#Agreement/Mediator/@ID =           IČO\n"+
+"contract#Agreement/Mediator/@Name =         Název\n"+
 "</xd:lexicon>";
 			xp = compile(new String[]{xdef, lexicon1, lexicon2});
-			xd = xp.createXDDocument("kontrakt");
+			xd = xp.createXDDocument("contract");
 			xml =
 "<Smlouva Číslo = \"0123456789\">\n"+
 "  <Klient Role       = \"1\"\n"+
@@ -134,7 +133,7 @@ public final class TestLexicon extends XDTester {
 			el = parse(xd, xml, reporter);
 			assertNoErrors(reporter);
 			assertEq(xml, el);
-			xd = xp.createXDDocument("kontrakt");
+			xd = xp.createXDDocument("contract");
 			xd.setLexiconLanguage("ces");
 			xml =
 "<Contract Number = \"0123456789\">\n"+
@@ -156,7 +155,7 @@ public final class TestLexicon extends XDTester {
 			assertNoErrors(reporter);
 			assertEq(xml, el);
 			xp = compile(new String[]{xdef, lexicon1, lexicon2});
-			xd = xp.createXDDocument("kontrakt");
+			xd = xp.createXDDocument("contract");
 			xml =
 "<Smlouva Číslo = \"0123456789\">\n"+
 "  <Klient Role       = \"1\"\n"+
@@ -176,13 +175,12 @@ public final class TestLexicon extends XDTester {
 			el = parse(xd, xml, reporter);
 			assertNoErrors(reporter);
 			assertEq(xml, el);
-
 			lexicon1 =
 "<xd:lexicon xmlns:xd='" + _xdNS + "' language='eng' default='yes'>\n"+
 "/* this is just a comment */\n"+
 "</xd:lexicon>";
 			xp = compile(new String[]{xdef, lexicon1, lexicon2});
-			xd = xp.createXDDocument("kontrakt");
+			xd = xp.createXDDocument("contract");
 			xml =
 "<Contract Number = \"0123456789\">\n"+
 "  <Client Typ  = \"1\"\n"+
@@ -202,12 +200,11 @@ public final class TestLexicon extends XDTester {
 			el = parse(xd, xml, reporter);
 			assertNoErrors(reporter);
 			assertEq(xml, el);
-
 			lexicon1 =
 "<xd:lexicon xmlns:xd='" + _xdNS + "' language='eng' default='yes'>\n"+
 "</xd:lexicon>";
 			xp = compile(new String[]{xdef, lexicon1, lexicon2});
-			xd = xp.createXDDocument("kontrakt");
+			xd = xp.createXDDocument("contract");
 			xml =
 "<Contract Number = \"0123456789\">\n"+
 "  <Client Typ  = \"1\"\n"+
@@ -266,11 +263,109 @@ public final class TestLexicon extends XDTester {
 			assertNoErrors(reporter);
 			el = xc.toXml();
 			assertEq(xml, el);
-			Method m = clazz.getDeclaredMethod("getNumber");
-			assertEq("0123456789", m.invoke(xc));
-			m = clazz.getDeclaredMethod("listOfClient");
-			List<?> list = (List<?>) m.invoke(xc);
-			assertEq(3, list.size());
+			assertEq("0123456789", SUtils.getValueFromGetter(xc,"getNumber"));
+			List<?> l = (List<?>)SUtils.getValueFromGetter(xc,"listOfClient");
+			assertEq(3, l.size());
+		} catch (Exception ex) {fail(ex);}
+		try {
+			xdef =
+"<xd:def xmlns:xd='" + _xdNS + "' root='Town' name='town'>\n"+
+"  <Town Name=\"string\">\n" +
+"     <Street xd:script=\"*;\" Name=\"string\">\n" +
+"        <House xd:script=\"*; ref _house\"/>\n" +
+"     </Street>\n" +
+"  </Town>\n" +
+"  <_house Num=\"int\" Address=\"optional string;\">\n" +
+"     <Person xd:script=\"*; ref _person\" />\n" +
+"  </_house>\n" +
+"  <_person FirstName=\"string\" LastName=\"string();\"/>\n" +
+"  <xd:lexicon language=\"eng\" default=\"yes\"/>\n" +
+"  <xd:lexicon language=\"deu\">\n" +
+"    town#Town =                 Stadt\n" +
+"    town#Town/@Name =           Name\n" +
+"    town#Town/Street =          Straße\n" +
+"    town#Town/Street/@Name =    Name\n" +
+"    town#Town/Street/House =    Haus\n" +
+"    town#_house/@Num =          Nummer\n" +
+"    town#_house/@Address =      Adresse\n" +
+"    town#_house/Person =        Person\n" +
+"    town#_person/@FirstName =   Vorname\n" +
+"    town#_person/@LastName =    Nachname\n" +
+"  </xd:lexicon>\n" +
+"\n" +
+"  <xd:lexicon language=\"ces\">\n" +
+"    town#Town =                 Město\n" +
+"    town#Town/@Name =           Jméno\n" +
+"    town#Town/Street =          Ulice\n" +
+"    town#Town/Street/@Name =    Jméno\n" +
+"    town#Town/Street/House =    Dům\n" +
+"    town#_house/@Num =          Číslo\n" +
+"    town#_house/@Address =      Adresa\n" +
+"    town#_house/Person =        Osoba\n" +
+"    town#_person/@FirstName =   Jméno\n" +
+"    town#_person/@LastName =    Příjmení\n" +
+"  </xd:lexicon>\n" +
+"  <xd:component>\n"+
+"    %class test.xdef.component.Town %link town#Town;\n"+
+"  </xd:component>\n"+
+"</xd:def>";
+			xp = compile(xdef);
+			genXComponent(xp, clearTempDir());
+			xd = xp.createXDDocument("town");
+			String xml_eng =
+"<Town Name='Nonehill'>\n" +
+"  <Street Name='Long'>\n" +
+"    <House Num='1'>\n" +
+"      <Person FirstName='John' LastName='Smith'></Person>\n" +
+"      <Person FirstName='Jane' LastName='Smith'></Person>\n" +
+"    </House>\n" +
+"    <House Num='2'/>\n" +
+"    <House Num='3'>\n" +
+"      <Person FirstName='James' LastName='Smith'></Person>\n" +
+"    </House>\n" +
+"  </Street>\n" +
+"  <Street Name='Short'>\n" +
+"    <House Num='1'>\n" +
+"      <Person FirstName='Jeremy' LastName='Smith'></Person>\n" +
+"    </House>\n" +
+"  </Street>\n" +
+"</Town>";
+			xd.setLexiconLanguage("eng");
+			el = parse(xd, xml_eng, reporter);
+			assertNoErrors(reporter);
+			assertEq(xml_eng, el);
+			xd = xp.createXDDocument("town");
+			xd.setLexiconLanguage("deu");
+			String xml_deu =
+"<Stadt Name='Nonehill'>\n" +
+"  <Straße Name='Long'>\n" +
+"    <Haus Nummer='1'>\n" +
+"      <Person Vorname='John' Nachname='Smith'></Person>\n" +
+"      <Person Vorname='Jane' Nachname='Smith'></Person>\n" +
+"    </Haus>\n" +
+"    <Haus Nummer='2'/>\n" +
+"    <Haus Nummer='3'>\n" +
+"      <Person Vorname='James' Nachname='Smith'></Person>\n" +
+"    </Haus>\n" +
+"  </Straße>\n" +
+"  <Straße Name='Short'>\n" +
+"    <Haus Nummer='1'>\n" +
+"      <Person Vorname='Jeremy' Nachname='Smith'></Person>\n" +
+"    </Haus>\n" +
+"  </Straße>\n" +
+"</Stadt>";
+			el = parse(xd, xml_deu, reporter);
+			assertNoErrors(reporter);
+			assertEq(xml_deu, el);
+			reporter.clear();
+			assertEq(xml_eng, xd.xtranslate(xml_deu, "deu", "eng", reporter));
+			assertNoErrors(reporter);
+			xd = xp.createXDDocument("town");
+			xd.setLexiconLanguage("deu");
+			XComponent xc = parseXC(xd, xml_deu, null, reporter);
+			assertNoErrors(reporter);
+			assertEq(xml_deu, xc.toXml());
+			assertEq("Nonehill", SUtils.getValueFromGetter(xc,"getName"));
 		} catch (Exception ex) {fail(ex);}
 
 		clearTempDir(); // delete temporary files.
