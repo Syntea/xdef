0571f94ee454a8b82d14abccd89b3e7919536e4d
Vßclav Trojan <trojan@syntea.cz>
2024-01-15 22:27:16 +0100

    .

--- xdef/src/main/java/org/xdef/impl/parsers/XDParseIPAddr.java
+++ xdef/src/main/java/org/xdef/impl/parsers/XDParseIPAddr.java
@@ -2,10 +2,13 @@ package org.xdef.impl.parsers;
 
 import org.xdef.XDParseResult;
 import org.xdef.XDParserAbstract;
+import static org.xdef.XDValueID.XD_IPADDR;
 import org.xdef.impl.code.DefIPAddr;
+import org.xdef.impl.code.DefParseResult;
 import org.xdef.msg.XDEF;
 import org.xdef.proc.XXNode;
 import org.xdef.sys.SParser;
+import org.xdef.xon.XonTools;
 
 /** Parse Internet IP address.
  * @author Vaclav Trojan
@@ -15,30 +18,34 @@ public class XDParseIPAddr extends XDParserAbstract {
 
 	public XDParseIPAddr() {super();}
 	@Override
-	public void parseObject(final XXNode xnode, final XDParseResult p) {
+	public void parseObject(final XXNode xn, final XDParseResult p) {
 		int pos = p.getIndex();
 		p.isSpaces();
-		p.isChar('/');
-		int pos1 = p.getIndex();
+		boolean quoted = xn != null && xn.getXonMode() > 0 && p.isChar('"');
+		XDParseResult q = quoted?new DefParseResult(XonTools.readJString(p)):p;
+		q.isChar('/');
+		int pos1 = q.getIndex();
 		int parts = 0;
 		while ("0123456789abcdefABCDEF".indexOf(p.getCurrentChar()) >= 0) {
-			p.nextChar();
-			char ch = p.isOneOfChars(":.");
+			q.nextChar();
+			char ch = q.isOneOfChars(":.");
 			if (ch != SParser.NOCHAR) {
 				parts++;
 				if(ch == ':') {
-					while(p.isChar(':')){}
+					while(q.isChar(':')){}
 				}
 			}
 		}
-		int pos2 = p.getIndex();
-		String s = "";
-		if (parts > 1) {
-			s = p.getBufferPart(pos1, pos2);
-			try {
-				p.setParsedValue(new DefIPAddr(s));
-				return;
-			} catch (Exception ex) {} //inet addr error
+		String s = q.getBufferPart(pos1, q.getIndex());
+		q.isSpaces();
+		if (q.eos()) {
+			if (parts > 1) {
+				try {
+					p.setParsedValue(new DefIPAddr(s));
+					p.setEos();
+					return;
+				} catch (Exception ex) {} //inet addr error
+			}
 		}
 		p.setIndex(pos);
 		p.setParsedValue(new DefIPAddr()); //null IPAddr
