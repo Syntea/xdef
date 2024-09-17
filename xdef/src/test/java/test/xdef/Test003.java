97b871a81ceb7cc2955cfbe2ae45c37732563f66
Vaclav Trojan <trojan@syntea.cz>
2024-09-17 17:05:21 +0200

    pom.xml: update properties to release version: 42.2.5

--- xdef/src/test/java/test/xdef/Test003.java
+++ xdef/src/test/java/test/xdef/Test003.java
@@ -6,15 +6,11 @@ import org.xdef.sys.ArrayReporter;
 import org.xdef.XDDocument;
 import org.xdef.XDFactory;
 import org.xdef.XDPool;
-import org.xdef.XDValue;
-import org.xdef.proc.XXData;
-import org.xdef.proc.XXNode;
 import org.xdef.sys.FileReportReader;
 import org.xdef.sys.FileReportWriter;
 import org.xdef.sys.ReportPrinter;
 import org.xdef.sys.ReportReader;
 import org.xdef.sys.ReportWriter;
-import org.xdef.sys.StringParser;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -37,113 +33,7 @@ import static test.XDTester.getFulltestMode;
  * @author Vaclav Trojan
  */
 public final class Test003 extends XDTester {
-	static int _count = 0;
-	boolean _myErrFlg;
-	int _xx;
 	public Test003() {super();}
-
-	public static boolean skodaTisice(final XXData xdata,final XDValue[]params){
-		Test003 test2 = (Test003)xdata.getUserObject();
-		if (test2 != null) {
-			test2._xx++;
-		}
-		StringParser p = new StringParser(xdata.getTextValue());
-		if (p.isInteger()) {
-			if (p.eos()) {
-				return true;
-			}
-			if (p.isChar(',')) {
-				if (p.isInteger()) {
-					if (p.eos()) {
-						return true;
-					}
-				}
-			}
-		}
-		xdata.error("XDEF532",
-			"Error detected by external check method: &{msg}",
-			"&{msg}skodaTisice: " + xdata.getTextValue());
-		return false;
-	}
-
-	public static boolean rodneCislo(final XXData xdata,
-		final XDValue[] params) {
-		try {
-			String value = xdata.getTextValue();
-			int rok = Integer.parseInt(value.substring(0,2));
-			int mesic = Integer.parseInt(value.substring(2,4));
-			if ((mesic < 1) || (mesic > 12 && mesic < 51) || (mesic > 62)) {
-				xdata.error("XDEF532",
-					"Error detected by external check method: &{msg}",
-					"&{msg}RC - mesic:" + mesic);
-				return false;
-			}
-			int den = Integer.parseInt(value.substring(4,6));
-			if ((den < 1) || (den > 31)) {
-				xdata.error("XDEF532",
-					"Error detected by external check method: &{msg}",
-					"&{msg}RC - mesic:" + mesic);
-				return false;
-			}
-			int i = 6;
-			if (value.charAt(i) == '/') {
-				i++;
-				if (Character.toUpperCase(value.charAt(i)) == 'C') {
-					return true; //cizinec
-				}
-			}
-			int n = Integer.parseInt(value.substring(i));
-			if (value.length() > i + 3) {
-				int mod =  n % 10;
-				int num = rok*10000000+mesic*100000+den*1000+(n/10);
-				if (num % 11 != mod) {
-					xdata.error("XDEF532",
-						"Error detected by external check method: &{msg}",
-						"&{msg}RC - CRC");
-					return false;
-				}
-			}
-			return true;
-		} catch (RuntimeException ex) {
-			xdata.error("XDEF532",
-				"Error detected by external check method: &{msg}",
-				"&{msg}RC - format:" + xdata.getTextValue());
-			return false;
-		}
-	}
-
-	public static boolean skodaText(final XXData xdata, final XDValue[] params){
-		if (xdata.getTextValue() != null) {
-			return true;
-		}
-		String s = xdata.getElement().getAttribute("TisKc");
-		if (s.length() > 0) {
-			xdata.setTextValue("");
-			return true;
-		}
-		return true;
-	}
-
-	public static boolean PlatnostPDN(final XDValue[] params) {return true;}
-
-	public static  boolean blobref(final XDValue[] params)	{return true;}
-
-	public static void myErr(XXNode chkElem, XDValue[] params) {
-		Test003 test2 = (Test003) chkElem.getUserObject();
-		if (test2 != null) {
-			test2._myErrFlg = true;
-		}
-		if ("/SouborD1A/ZaznamPDN[54]/VyliceniDN[1]".equals(chkElem.getXPos())){
-			chkElem.error(null,"OK Error myErr: " + chkElem.getXPos());
-		} else  {
-			chkElem.error(null, "Incorrect Error myErr: " + chkElem.getXPos());
-			chkElem.error(null,"Error myErr again");
-		}
-	}
-
-	public static void trString(final XDValue[] params) {}
-
-	public static void konecZaznamu(final XDValue[] params)	{_count++;}
 
 	@Override
 	/** Run tests. */
@@ -189,8 +79,8 @@ public final class Test003 extends XDTester {
 			xd.setUserObject(this);
 			el = xd.xparse(dataDir + "SouborD1D.xml", rw);
 			if (null != xd.getDocument().getXmlEncoding()
-				&& !"UTF-8".equalsIgnoreCase(
-				el.getOwnerDocument().getXmlEncoding())) {
+				&& !"UTF-8".equalsIgnoreCase(el.getOwnerDocument()
+					.getXmlEncoding())) {
 				fail("encoding: " + xd.getDocument().getXmlEncoding());
 			}
 			isr = new InputStreamReader(
@@ -211,8 +101,133 @@ public final class Test003 extends XDTester {
 			isr.close();
 			lst.close();
 		} catch (IOException | RuntimeException ex) {fail(ex);}
+		try {// check compiling if source items have assignment of sourceId
+			Object[] p1 = new Object[] { // sources
+"<xd:def  xmlns:xd='" + _xdNS + "' root='A' name='A' ><A/></xd:def>",
+"<xd:def xmlns:xd='" + _xdNS + "' root='B' name='B' ><B/></xd:def>",
+			new ByteArrayInputStream((
+"<xd:def xmlns:xd='" + _xdNS + "' root='C' name='C' ><C/></xd:def>")
+				.getBytes("UTF-8"))};
+			String[] p2 = new String[] {"AA", "AB", "AC"}; // source names
+			xp = XDFactory.compileXD(null, p1, p2);
+			assertEq(xml = "<A/>", parse(xp, "A", xml, reporter));
+			assertNoErrorwarnings(reporter);
+			assertEq(xml = "<B/>", parse(xp, "B", xml, reporter));
+			assertNoErrorwarnings(reporter);
+			assertEq(xml = "<C/>", parse(xp, "C", xml, reporter));
+			assertNoErrorwarnings(reporter);
+		} catch (UnsupportedEncodingException | RuntimeException ex) {fail(ex);}
+		try {// test URL with encoded chracters.
+			File f = new File(clearTempDir(), "/aa bb");
+			f.mkdirs();
+			f = new File(f, "čé řž.xdef");
+			Writer wr = new OutputStreamWriter(new FileOutputStream(f),"UTF-8");
+			wr.write(
+"<xd:def xmlns:xd='" + _xdNS + "' name=\"A\" root=\"test\">\n" +
+"<test a='int()'/>\n" +
+"</xd:def>");
+			wr.close();
+			URL u = f.toURI().toURL();
+			XDFactory.compileXD(null, u);
+			xdef =
+"<xd:collection xmlns:xd='" + _xdNS + "' include='"+u.toExternalForm()+"' />";
+			xp = compile(xdef);
+
+			xml = "<test a='123'/>";
+			f = new File(clearTempDir(), "/aa bb");
+			f.mkdirs();
+			f = new File(f, "čé řž.xml");
+			wr = new OutputStreamWriter(new FileOutputStream(f),"UTF-8");
+			wr.write(xml);
+			wr.close();
+			xp.createXDDocument("A");
+			assertEq(xml, parse(xp,"A",xml, reporter));
+			assertNoErrorsAndClear(reporter);
+			xd = xp.createXDDocument("A");
+			assertEq(xml, xd.xparse(f.toURI().toURL(), reporter));
+			assertNoErrorsAndClear(reporter);
+		} catch (IOException | RuntimeException ex) {fail(ex);}
+		try {// join elements "B" which can be joined.
+			xdef =
+"<xd:def xmlns:xd='" + _xdNS + "' root = 'A'>\n"+
+"<xd:declaration>\n"+
+"  /** Test if the element f can be joined with the element e. */\n" +
+"  boolean join(Element e, Element f, String a, String b, String mask) {\n"+
+"    Datetime t1 = new Datetime(e.getAttribute(b));\n"+
+"    if (mask.endsWith(\"d\")) {\n" +
+"      t1 = t1.addDay(1);\n" +
+"    } else if (mask.endsWith(\"H\")) {\n" +
+"      t1 = t1.addHour(1);\n" +
+"    } else if (mask.endsWith(\"s\")) {\n" +
+"      t1 = t1.addSecond(1);\n" +
+"    } else {\n" +
+"       return false; /*can't join*/\n" +
+"    }\n" +
+"    Datetime t2 = new Datetime(f.getAttribute(a));\n"+
+"    Container eatrs = e.getAttributes();\n" +
+"    Container fatrs = f.getAttributes();\n" +
+"    if (!t1.equals(t2) || eatrs.getLength() != fatrs.getLength()\n" +
+"      || !e.hasAttribute(a) || !f.hasAttribute(a)\n" +
+"      || !e.hasAttribute(b) || !f.hasAttribute(b)) {\n" +
+"      return false; /*can't join*/\n"+
+"    }\n"+
+"    for (int i = 0; i LT eatrs.getLength(); i++) {\n" +
+"      String name = ((NamedValue) eatrs.item(i)).getName();\n" +
+"      if (!a.equals(name) AAND !b.equals(name) AAND\n" +
+"		 !e.getAttribute(name).equals(f.getAttribute(name)))\n" +
+"		 return false; /*can't join*/\n" +
+"    }\n" +
+"    return true;\n" +
+"  }\n"+
+"  /** Create list of joined elements. */\n" +
+"  Container x(Container x, String a, String b, String mask) {\n"+
+"    Container y = new Container();\n" +
+"    if (!x.isEmpty()) {\n" +
+"      Element e = x.getElement(0);\n" +
+"      y.addItem(e);\n" +
+"      for (int i = 1; i LT x.getLength(); i++) {\n" +
+"        Element f = x.getElement(i);\n" +
+"        if (join(e, f, a, b, mask)) {\n" +
+"          e.setAttribute(b, f.getAttribute(b));\n" +
+"        } else {\n" +
+"          y.addItem(e = f);\n" +
+"        }\n" +
+"      }\n" +
+"    }\n" +
+"    return y;\n" +
+"  }\n"+
+"</xd:declaration>\n"+
+"<A>\n"+
+"  <B xd:script=\"occurs *; create x(from('//B'), 'x', 'y', 'yyyy-MM-dd');\"\n"+
+"    a='string' b='string'\n"+
+"    x=\"xdatetime('yyyy-MM-dd')\" y=\"xdatetime('yyyy-MM-dd')\"/>\n"+
+"</A>\n"+
+"</xd:def>";
+		xml =
+"<A>\n"+
+"  <B a='a' b='b' x='2023-12-08' y='2023-12-31'/>\n"+
+"  <B a='a' b='b' x='2024-01-01' y='2024-09-11'/>\n"+
+"  <B a='a' b='b' x='2024-09-12' y='2024-09-13'/>\n"+
+"  <B a='a' b='b' x='2024-09-20' y='2024-09-30'/>\n"+
+"  <B a='a' b='b' x='2024-10-01' y='2024-10-02'/>\n"+
+"</A>";
+			xd = compile(xdef).createXDDocument();
+			xd.setXDContext(xml);
+			el = xd.xcreate("A", null);
+			KXmlUtils.compareElements(el,
+"<A>" +
+"<B a=\"a\" b=\"b\" x=\"2023-12-08\" y=\"2024-09-13\"/>" +
+"<B a=\"a\" b=\"b\" x=\"2024-09-20\" y=\"2024-10-02\"/>" +
+"</A>").checkAndThrowErrorWarnings();
+			assertEq(el,
+"<A>\n" +
+"  <B a=\"a\" b=\"b\" x=\"2023-12-08\" y=\"2024-09-13\"/>\n" +
+"  <B a=\"a\" b=\"b\" x=\"2024-09-20\" y=\"2024-10-02\"/>\n" +
+"</A>");
+		} catch (RuntimeException ex) {fail(ex);}
+		 // test big XML
 		if (getFulltestMode()) {
-			try { // test big XML
+			try {
 				xdef =
 "<xd:def xmlns:xd='" + _xdNS + "' xd:root=\"koně\">\n"+
 "  <koně>\n"+
@@ -265,146 +280,6 @@ public final class Test003 extends XDTester {
 					+ "s (" + df.format((datalen / 1000.0)/duration)+"KB/s);");
 			} catch (IOException | RuntimeException ex) {fail(ex);}
 		}
-		try {// check compiling if source items have assignment of sourceId
-			Object[] p1 = new Object[] { // sources
-"<xd:def  xmlns:xd='" + _xdNS + "' root='A' name='A' ><A/></xd:def>",
-"<xd:def xmlns:xd='" + _xdNS + "' root='B' name='B' ><B/></xd:def>",
-			new ByteArrayInputStream((
-"<xd:def xmlns:xd='" + _xdNS + "' root='C' name='C' ><C/></xd:def>")
-				.getBytes("UTF-8"))};
-			String[] p2 = new String[] {"AA", "AB", "AC"}; // source names
-			xp = XDFactory.compileXD(null, p1, p2);
-			assertEq(xml = "<A/>", parse(xp, "A", xml, reporter));
-			assertNoErrorwarnings(reporter);
-			assertEq(xml = "<B/>", parse(xp, "B", xml, reporter));
-			assertNoErrorwarnings(reporter);
-			assertEq(xml = "<C/>", parse(xp, "C", xml, reporter));
-			assertNoErrorwarnings(reporter);
-		} catch (UnsupportedEncodingException | RuntimeException ex) {fail(ex);}
-		try {// test URL with encoded chracters.
-			File f = new File(clearTempDir(), "/aa bb");
-			f.mkdirs();
-			f = new File(f, "čé řž.xdef");
-			Writer wr = new OutputStreamWriter(new FileOutputStream(f),"UTF-8");
-			wr.write(
-"<xd:def xmlns:xd='" + _xdNS + "' name=\"A\" root=\"test\">\n" +
-"<test a='int()'/>\n" +
-"</xd:def>");
-			wr.close();
-			URL u = f.toURI().toURL();
-			XDFactory.compileXD(null, u);
-			xdef =
-"<xd:collection xmlns:xd='" + _xdNS + "' include='"+u.toExternalForm()+"' />";
-			xp = compile(xdef);
-
-			xml = "<test a='123'/>";
-			f = new File(clearTempDir(), "/aa bb");
-			f.mkdirs();
-			f = new File(f, "čé řž.xml");
-			wr = new OutputStreamWriter(new FileOutputStream(f),"UTF-8");
-			wr.write(xml);
-			wr.close();
-			xp.createXDDocument("A");
-			assertEq(xml, parse(xp,"A",xml, reporter));
-			assertNoErrorsAndClear(reporter);
-			xd = xp.createXDDocument("A");
-			assertEq(xml, xd.xparse(f.toURI().toURL(), reporter));
-			assertNoErrorsAndClear(reporter);
-		} catch (IOException | RuntimeException ex) {fail(ex);}
-		try {// Join elements "B" which can be joined.
-			xdef =
-"<xd:def xmlns:xd='" + _xdNS + "' root = 'A'>\n"+
-"<xd:declaration>\n"+
-"  /** Test if the element f can be joined with the element e.\n" +
-"   * @param e first element.\n" +
-"  * @param f second element.\n" +
-"   * @param a name of attribute with datetime \"start\"\n" +
-"   * @param b name of attribute with datetime \"end\"\n" +
-"   * @param mask description of datetime format.\n" +
-"   * @return true if and only if f can be joined with e.\n" +
-"   */\n" +
-"  boolean join(Element e, Element f, String a, String b, String mask) {\n"+
-"    Datetime t1 = new Datetime(e.getAttribute(b));\n"+
-"    Datetime t2 = new Datetime(f.getAttribute(a));\n"+
-"    if (mask.endsWith(\"d\")) {\n" +
-"      t1 = t1.addDay(1);\n" +
-"    } else if (mask.endsWith(\"H\")) {\n" +
-"      t1 = t1.addHour(1);\n" +
-"    } else if (mask.endsWith(\"s\")) {\n" +
-"      t1 = t1.addSecond(1);\n" +
-"    } else {\n" +
-"       return false; /*can't join*/\n" +
-"    }\n" +
-"    Container eatrs = e.getAttributes();\n" +
-"    Container fatrs = f.getAttributes();\n" +
-"    if (!t1.equals(t2) || !e.hasAttribute(a) || !f.hasAttribute(a)\n" +
-"      || !e.hasAttribute(b) || !f.hasAttribute(b)\n" +
-"      || eatrs.getLength() != fatrs.getLength()) {\n" +
-"      return false; /*can't join*/\n"+
-"    }\n"+
-"    for (int i = 0; i LT eatrs.getLength(); i++) {\n" +
-"      String name = ((NamedValue) eatrs.item(i)).getName();\n" +
-"      if (!a.equals(name) AAND !b.equals(name) AAND\n" +
-"		 !e.getAttribute(name).equals(f.getAttribute(name)))\n" +
-"		 return false; /*can't join*/\n" +
-"    }\n" +
-"    return true;\n" +
-"  }\n"+
-"\n"+
-"  /** Create list of joined elements.\n" +
-"   * @param x container with elements.\n" +
-"   * @param a name of attribute with datetime \"start\"\n" +
-"   * @param b name of attribute with datetime \"end\"\n" +
-"   * @param mask description of datetime format.\n" +
-"   * @return Container with joined elements.\n" +
-"   */\n" +
-"  Container x(Container x, String a, String b, String mask) {\n"+
-"    Container y = new Container();\n" +
-"    if (!x.isEmpty()) {\n" +
-"      Element e = x.getElement(0);\n" +
-"      y.addItem(e);\n" +
-"      for (int i = 1; i LT x.getLength(); i++) {\n" +
-"        Element f = x.getElement(i);\n" +
-"        if (join(e, f, a, b, mask)) {\n" +
-"          e.setAttribute(b, f.getAttribute(b));\n" +
-"        } else {\n" +
-"          y.addItem(e = f);\n" +
-"        }\n" +
-"      }\n" +
-"    }\n" +
-"    return y;\n" +
-"  }\n"+
-"</xd:declaration>\n"+
-"\n"+
-"<A>\n"+
-"  <B xd:script=\"occurs *; create x(from('//B'), 'x', 'y', 'yyyy-MM-dd');\"\n"+
-"  a='string' b='string'\n"+
-"     x=\"xdatetime('yyyy-MM-dd')\" y=\"xdatetime('yyyy-MM-dd')\"/>\n"+
-"</A>\n"+
-"\n"+
-"</xd:def>";
-		xml =
-"<A>\n"+
-"  <B a='a' b='b' x='2023-12-08' y='2023-12-31'/>\n"+
-"  <B a='a' b='b' x='2024-01-01' y='2024-09-11'/>\n"+
-"  <B a='a' b='b' x='2024-09-12' y='2024-09-13'/>\n"+
-"  <B a='a' b='b' x='2024-09-20' y='2024-09-30'/>\n"+
-"  <B a='a' b='b' x='2024-10-01' y='2024-10-02'/>\n"+
-"</A>";
-			xd = compile(xdef).createXDDocument();
-			xd.setXDContext(xml);
-			el = xd.xcreate("A", null);
-			KXmlUtils.compareElements(el,
-"<A>" +
-"<B a=\"a\" b=\"b\" x=\"2023-12-08\" y=\"2024-09-13\"/>" +
-"<B a=\"a\" b=\"b\" x=\"2024-09-20\" y=\"2024-10-02\"/>" +
-"</A>").checkAndThrowErrorWarnings();
-			assertEq(el,
-"<A>\n" +
-"  <B a=\"a\" b=\"b\" x=\"2023-12-08\" y=\"2024-09-13\"/>\n" +
-"  <B a=\"a\" b=\"b\" x=\"2024-09-20\" y=\"2024-10-02\"/>\n" +
-"</A>");
-		} catch (RuntimeException ex) {fail(ex);}
 
 		resetTester();
 	}
