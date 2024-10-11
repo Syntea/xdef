e42157e77dd5aad6bd7ff6f62cc3e185b067034b
Vßclav Trojan <trojan@syntea.cz>
2024-10-11 20:45:48 +0200

    .

--- xdef/src/main/java/org/xdef/xon/XonYaml.java
+++ xdef/src/main/java/org/xdef/xon/XonYaml.java
@@ -9,8 +9,8 @@ import org.xdef.msg.JSON;
 import org.xdef.sys.SRuntimeException;
 
 /** Tools for YAML.
- * Processing YAML objects requires the availability of the org.yaml.snakeyaml
- * package in the classpath.
+ * Processing YAML objects requires the org.yaml.snakeyaml package
+ * in the classpath.
  * @author Vaclav Trojan
  */
 public final class XonYaml {
@@ -43,7 +43,9 @@ public final class XonYaml {
 			} catch (IllegalAccessException | IllegalArgumentException
 				| InstantiationException | NoSuchMethodException
 				| SecurityException | InvocationTargetException ex) {
-				throw new RuntimeException(ex);
+				throw new RuntimeException(
+					"Error when creating instance of org.yaml.snakeyaml.Yaml",
+					ex);
 			}
 		}
 	}
@@ -52,12 +54,10 @@ public final class XonYaml {
 		prepareYAML();
 		try {
 			return (String) _yamlDump.invoke(_yaml, XonUtils.xonToJson(o));
-		} catch (SRuntimeException ex) {
-			throw ex;
 		} catch (IllegalAccessException | IllegalArgumentException
 			| InvocationTargetException ex) {
-			throw new RuntimeException(
-				"Error when creating Yaml string from object", ex);
+			throw new RuntimeException(ex.getCause() != null
+				&& ex.getCause().getMessage() != null ? ex.getCause() : ex);
 		}
 	}
 
@@ -70,12 +70,10 @@ public final class XonYaml {
 		prepareYAML();
 		try {
 			return _yamlLoadReader.invoke(_yaml, source);
-		} catch (SRuntimeException ex) {
-			throw ex;
 		} catch (IllegalAccessException | IllegalArgumentException
 			| InvocationTargetException ex) {
-			throw new RuntimeException(
-				"Error when parsing Yaml from Reader", ex);
+			throw new RuntimeException(ex.getCause() != null
+				&& ex.getCause().getMessage() != null ? ex.getCause() : ex);
 		}
 	}
 
@@ -83,12 +81,38 @@ public final class XonYaml {
 		prepareYAML();
 		try {
 			return _yamlLoadInputStream.invoke(_yaml, source);
-		} catch (SRuntimeException ex) {
-			throw ex;
 		} catch (IllegalAccessException | IllegalArgumentException
 			| InvocationTargetException ex) {
-			throw new RuntimeException(
-				"Error when parsing Yaml from InputStream", ex);
+			throw new RuntimeException(ex.getCause() != null
+				&& ex.getCause().getMessage() != null ? ex.getCause() : ex);
 		}
+	}
+	
+	/** Convert YAML source format of data X-definition model to JSON format. 
+	 * @param yaml string with YAML format of data X-definition model.
+	 * @return JSON format of data X-definition model.
+	 */
+	public static final String yamlToJsonXScript(final String yaml) {
+		Object x = XonUtils.parseYAML(yaml);
+		String s = XonUtils.toJsonString(XonUtils.xonToJson(x), true);
+		String[] keys = {"%anyName", "%anyObj", "%script", "%oneOf"};
+		for (String key : keys) {
+			int ndx; // index of the key in the source
+			while ((ndx = s.indexOf("\"" + key)) >= 0) {
+				int ndx1 = ndx + key.length() + 1;
+				if (ndx1 >= s.length()) break;
+				switch(s.charAt(ndx1++)) {
+					case '=':
+						s = s.substring(0, ndx) + key+"=\"" + s.substring(ndx1);
+						break;
+					case '"':
+						s = ("%script".equals(key) && ndx1 < s.length()
+							&& s.charAt(ndx1) == ':') //%script: -> %script=
+							? s.substring(0,ndx) + key+"=" + s.substring(++ndx1)
+							: s.substring(0, ndx) + key + s.substring(ndx1);
+				}
+			}
+		}
+		return s.endsWith("\n") ? s : (s + '\n');
 	}
 }
