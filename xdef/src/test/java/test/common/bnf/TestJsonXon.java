2389f48f8400a1d7edb6de2a4aaf9d7b3f8dadb1
Vaclav Trojan <trojan@syntea.cz>
2024-10-15 19:02:45 +0200

    .

--- xdef/src/test/java/test/common/bnf/TestJsonXon.java
+++ xdef/src/test/java/test/common/bnf/TestJsonXon.java
@@ -3,12 +3,13 @@ package test.common.bnf;
 import java.io.File;
 import org.xdef.sys.BNFGrammar;
 import org.xdef.sys.FUtils;
+import org.xdef.sys.SException;
 import static org.xdef.sys.STester.runTest;
 import org.xdef.sys.SUtils;
 import org.xdef.sys.StringParser;
 import test.XDTester;
 
-/** Test XDefinition script.
+/** Test JSON and XON data.
  * @author Vaclav Trojan
  */
 public class TestJsonXon extends XDTester {
@@ -23,6 +24,61 @@ public class TestJsonXon extends XDTester {
 		
 	}
 	
+	/** Test incorrect data.
+	 * @param x array with data items.
+	 * @param g BNF grammar.
+	 * @param rule rule name.
+	 */
+	private void testfail(final String[] x,
+		final BNFGrammar g,
+		final String rule){
+		for (String t : x) {
+			String s = parse(g, rule, t);
+			if (t.equals(s)) {
+				fail("Error not recognized:\n" + s + "\n" + t); 
+			}
+		}
+	}
+	
+	/** Check correct data.
+	 * @param x array with data items.
+	 * @param g BNF grammar.
+	 * @param rule rule name.
+	 */
+	private void testx(final String[] x, final BNFGrammar g, final String rule){
+		for (String t : x) {
+			String s = parse(g, rule, t);
+			if (!t.equals(s)) {
+				fail("E:\n" + s + "\n" + t); 
+			}
+		}
+	}
+
+	/** Check files with correct data.
+	 * @param x array with data files.
+	 * @param g BNF grammar.
+	 * @param rule rule name.
+	 */
+	private void testx(final File[] x, final BNFGrammar g, final String rule) {
+		for (File f: x) {
+			try {
+				String name = f.getName();
+				int ndx = name.indexOf("UTF");
+				String t = name.equals("Test201windows-1250.xon")
+					? FUtils.readString(f, "windows-1250")
+					: ndx > 0 ? FUtils.readString(f,
+						name.substring(ndx, name.indexOf(".xon")))
+					: FUtils.readString(f, "UTF-8");
+				String s = parse(g, rule, t);
+				if (!t.equals(s)) {
+					fail("E: " + f.getName() + "\n" + s + "\n" + t); 
+				}
+			} catch (SException ex) {
+				fail("E: " + f.getName() + "\n" + ex); 
+			}
+		}
+	}
+		
 	private String parse(BNFGrammar grammar, String name, String source) {
 		StringParser p = new StringParser(source);
 		if (grammar.parse(p, name)) {
@@ -42,11 +98,11 @@ public class TestJsonXon extends XDTester {
 		String bnf;
 		BNFGrammar g;
 		String s;
-		try { // JSON
-			bnf =
-"S            ::= [#9#10#13 ]+ /*whitespaces*/\n" +
+		try {
+			bnf =  // JSON
+"S            ::= [#9#10#13 ]+ /*whitespace*/\n" +
 "IntNumber    ::= $digit ($digit)*\n" +
-"SignedInteger::= '-'? IntNumber\n" +
+"SignedInteger::= '-'? ('0' | [1-9] [0-9]*)\n" +
 "Exponent     ::= ('e' | 'E') ('+' | '-')? IntNumber\n" +
 "FloatPart    ::= '.' IntNumber Exponent? | Exponent\n" +
 "FloatNumber  ::= SignedInteger FloatPart\n" +
@@ -69,38 +125,40 @@ public class TestJsonXon extends XDTester {
 "Value        ::= Array | Map | SimpleValue\n" + 
 "json         ::= Value S?\n";
 			g = BNFGrammar.compile(bnf);
-			assertEq(s="null", parse(g,"json",s));
-			assertEq(s="false", parse(g,"json",s));
-			assertEq(s="\"\"", parse(g,"json",s));
-			assertEq(s="\"a\"", parse(g,"json",s));
-			assertEq(s="\"\\u0045\\n\\t\\\\\\\"\\b\"", parse(g,"json",s));
-			assertEq(s="1", parse(g,"json",s));
-			assertEq(s="-3.14e+10", parse(g,"json",s));
-			assertEq(s="[]", parse(g,"json",s));
-			assertEq(s="{}", parse(g,"json",s));
-			assertEq(s="[ null ]", parse(g,"json",s));
-			assertEq(s="[1e-3]", parse(g,"json",s));
-			assertEq(s="[ 1.5, true, \"\", [[]], {} ]", parse(g,"json",s));
-			assertEq(s="{\"a\" : 1e2}", parse(g,"json",s));
-			assertEq(s="{ \"a\" :{} }", parse(g,"json",s));
-			assertEq(s="{\"a\":{},\"b\":[] , \"c\":null}", parse(g,"json",s));
-			assertEq(parse(g,"json","aABB"),
-				"json failed, Line: 1, column: 1, position: 0, startLine: 0;");
-			s = getXonDir();
-			System.out.println(s);
-			for (File f: SUtils.getFileGroup(s + "Test*.json")) {
-				String t = FUtils.readString(f, "UTF-8");
-				String u = parse(g, "json", t);
-				if (!t.equals(u)) {
-					fail("E: " + f.getName() + "\n" + u + "\n" + t); 
-				}
-			}
-		} catch (Exception ex) {fail(ex);}
-		try { // XON
-			bnf =
-"S            ::= [#9#10#13 ]+ /*whitespaces*/\n" +
+			testx(new String[] {
+				"null",
+				"false",
+				"1",
+				"-3.14e+10",
+				"\"\"",
+				"\"a\"",
+				"\"\\u0045\\n\\t\\\\\\\"\\b\"",
+				"[]",
+				"{}",
+				"[ null ]",
+				"[1e-3]",
+				"[ 1.5, true, \"\", [[]], {} ]",
+				"{\"a\" : 1e2}",
+				"{ \"a\" :{} }",
+				"{\"a\":{},\"b\":[] , \"c\":null}"}, g, "json");
+			testx(SUtils.getFileGroup(getXonDir() + "Test*.json"), g, "json");
+			testfail(new String[] { // test JSON incorrect data
+				"aABB",
+				"\"\\A\"",
+				"\"\\u0xyz\"",
+				"[1,2,]",
+				"{\"\":1,}",
+				"[01.1]",
+				"[01]",
+				"[1.]",
+				"[.1]"}, g, "json");
+
+			bnf = // XON
+"WhiteSpace ::= [#9#10#13 ]\n" +
+"Comment ::= \"/*\" ([^*]+ | \"*\" - \"*/\")* \"*/\"\n" +
+"S ::= (WhiteSpace | Comment)+ /* Sequence of whitespaces or comments */\n" +
 "IntNumber    ::= $digit ($digit)*\n" +
-"SignedInteger::= '-'? IntNumber\n" +
+"SignedInteger::= '-'? ('0' | [1-9] [0-9]*)\n" +
 "Exponent     ::= ('e' | 'E') ('+' | '-')? IntNumber\n" +
 "FloatPart    ::= '.' IntNumber Exponent? | Exponent\n" +
 "FloatNumber  ::= SignedInteger FloatPart\n" +
@@ -110,8 +168,8 @@ public class TestJsonXon extends XDTester {
 "Long         ::= SignedInteger 'l' ?\n" +
 "BigInteger   ::= SignedInteger 'N' \"l\" ?\n" +
 "Decimal      ::= SignedInteger 'D' | FloatNumber 'D'\n" +
-"Float        ::= SignedInteger 'f' | FloatNumber 'f' | 'NaNf' | '-' ? 'INFf'\n" +
-"Double       ::= SignedInteger 'd' | FloatNumber 'd'? | 'NaN' | '-' ? 'INF'\n" +
+"Float        ::= SignedInteger 'f' | FloatNumber 'f' | 'NaNf' | '-'? 'INFf'\n"+
+"Double       ::= SignedInteger 'd' | FloatNumber 'd'? | 'NaN' | '-'? 'INF'\n" +
 "yearFrag     ::= '-'? $digit*\n" +
 "monthFrag    ::= ('0' [1-9]) | ('1' [0-2])\n" +
 "dayFrag      ::= ('0' [1-9]) | ([12] $digit) | ('3' [01])\n" +
@@ -120,13 +178,20 @@ public class TestJsonXon extends XDTester {
 "secondFrag   ::= ([0-5] $digit) ('.' $digit+)?\n" +
 "endOfDayFrag ::= '24:00:00' ('.' '0'+)?\n" +
 "timezoneFrag ::= 'Z' | ('+' | '-') (('0' $digit | '1' [0-3])\n" +
-"             (':' minuteFrag )? | '14:00')\n" +
-"dateFrag     ::= yearFrag | yearFrag '-' monthFrag ( '-' dayFrag)?\n" +
-"             | '--' monthFrag? '-' dayFrag | '--' monthFrag\n" +
+"             (':' minuteFrag )? | '14:00'\n"+
+"             | ('0' $digit | '1' [0-3]) [0-5] $digit | '1400')\n" +
+"dateFrag     ::= yearFrag | yearFrag '-' monthFrag ('-' dayFrag)?\n" +
+"             | '--' monthFrag | '--' monthFrag '-' dayFrag\n" +
+"             | '---' dayFrag\n" +
+"/* dateFrag and zone */\n" +
+"dateFragZ    ::= yearFrag | yearFrag '-' monthFrag\n" +
+"             ('-' dayFrag)? timezoneFrag? | '--' monthFrag timezoneFrag?\n" +
+"             | '--' monthFrag '-' dayFrag timezoneFrag?\n" +
+"             | '---' dayFrag timezoneFrag?\n" +
+"date         ::= 'd' dateFragZ\n" +
 "timeFrag     ::= ( (hourFrag ':' minuteFrag ':' secondFrag)\n" +
 "             | endOfDayFrag ) | (hourFrag ':' minuteFrag)\n" +
 "time         ::= 'd' timeFrag timezoneFrag?\n" +
-"date         ::= 'd' dateFrag  timezoneFrag?\n" +
 "dateTime     ::= 'd' dateFrag 'T' (timeFrag | endOfDayFrag) timezoneFrag?\n" +
 "duYearFrag   ::= $digit+ 'Y'\n" +
 "duMonthFrag  ::= $digit+ 'M'\n" +
@@ -160,41 +225,27 @@ public class TestJsonXon extends XDTester {
 "Directive    ::= S? '%encoding' S? '=' S? '\"' CharsetName '\"' S?\n" +
 "xon          ::=  Directive? Value S?\n";
 			g = BNFGrammar.compile(bnf);
-			assertEq(s="null", parse(g, "xon", s));
-			assertEq(s="false", parse(g, "xon", s));
-			assertEq(s="\"\"", parse(g, "xon", s));
-			assertEq(s="\"a\"", parse(g, "xon", s));
-			assertEq(s="\"\\u0045\\n\\t\\\\\\\"\\b\"", parse(g, "xon", s));
-			assertEq(s="1", parse(g, "xon", s));
-			assertEq(s="-3.14e+10", parse(g, "xon", s));
-			assertEq(s="0N", parse(g, "xon", s));
-			assertEq(s="[]", parse(g, "xon", s));
-			assertEq(s="{}", parse(g, "xon", s));
-			assertEq(s="[ null ]", parse(g, "xon", s));
-			assertEq(s="[1e-3]", parse(g, "xon", s));
-			assertEq(s="[ 1.5, true, \"\", [[]], {} ]", parse(g, "xon", s));
-			assertEq(s="{\"a\": 1e2}", parse(g, "xon", s));
-			assertEq(s="{ a :{}, b:[], c:null }", parse(g, "xon", s));
-			s = "[1, { a : 1, b:\"a\", \"\":false, array:[], map:{}}, \"abc\"]";
-			assertEq(s, parse(g, "xon", s));
-			assertEq(s, parse(g, "xon", s));
-			assertEq(parse(g, "xon", "aABB"),
-				"xon failed, Line: 1, column: 1, position: 0, startLine: 0;");
-			for (String t : new String[] {
-				"true",
+			testx(new String[] {
 				"\"true\"",
 				"null",
 				"1l", //long
+				"\"\"", // empty string
+				"\"\\u0045\"", // string with UTFChar
+				"\"\\n\\t\\\\\\\"\\b\"", // esccaped chars
 				"-1.25e-3f",
 				"d1949-11-07",
 				"d1949-11-07T15:59Z",
 				"d1949-11-07T15:59:01.123",
 				"d1949-11-07T15:59:01.123-02:00",
+				"d1949-11-07T15:59:01.123-0200",
 				"d1949-11-07T15:59:01.123-02",
 				"d1945", //gYear
 				"d---29", //gDay
+				"d---29-02:00", //gDay
 				"d--12", //gMonth
+				"d--12-02:00", //gMonth
 				"d--12-29", //gMonthDay
+				"d--12-29-02:00", //gMonthDay
 				"d11:20",
 				"d11:20:31",
 				"d11:20:31.123",
@@ -202,6 +253,10 @@ public class TestJsonXon extends XDTester {
 				"d11:20:31.123-02:00",
 				"d11:20+02:00",
 				"d11:20:31-02:00",
+				"P2Y6M5DT12H35M30S",
+				"P1Y2M3DT10H30M123.123456S",
+				"-P1DT2H",
+				"P1Y1M1DT1H1M1.1234567S",
 				"{}",
 				"{\"\":\"\"}",
 				"{\"A B\":{\"a b\":\"\"}}",
@@ -219,32 +274,33 @@ public class TestJsonXon extends XDTester {
 				"[{a:[{}]}]",
 				"[{a:[{a:1}]}]",
 				"[1, -1.25e-3, true, \"abc\", null]",
+				"{ a :{}, b:[], c:null }",
 				"[[3,null,false],[3.14,\"\",false],[\"aa\",true,false]]",
 				"[1, { _x69_:1, _x5f_map:null, \"a_x\tb\":[null], item:{}}]",
-				"[1,{ a:1, b:\"a\", \"\":false, array : [], map:{}}, \"abc\"]",
+				"[1, { a : 1, b:\"a\", \"\":false, array:[], map:{}}, \"abc\"]",
 				"[{a:[{},[1,2],{},[3,4]]}]",
 				"[{a:[{a:1},[1,2],{},[3,4]]}]",
 				"[{a:[[1,2],{},[3,4]]}]",
 				"[{a:[[1,2],{a:1},[30,4]]}]",
 				"[{a:[[1,2],[3,4],{}]}]",
-				"[{a:[[1,2],[3,4],{a:1,b:2}]}]",
-				}) {
-				assertEq(t, parse(g, "xon", t));
-			}
-			s = getXonDir();
-			for (File f: SUtils.getFileGroup(s + "Test*.xon")) {
-				String name = f.getName();
-				int ndx = name.indexOf("UTF");
-				String t = name.equals("Test201windows-1250.xon")
-					? FUtils.readString(f, "windows-1250")
-					: ndx > 0 ? FUtils.readString(f,
-						name.substring(ndx, name.indexOf(".xon")))
-					: FUtils.readString(f, "UTF-8");
-				String u = parse(g, "xon", t);
-				if (!t.equals(u)) {
-					fail("E: " + f.getName() + "\n" + u + "\n" + t); 
-				}
-			}			
+				"[{a:[[1,2],[3,4],{a:1,b:2}]}]"}, g, "xon");
+			testx(SUtils.getFileGroup(getXonDir() + "Test*.xon"),  g, "xon");
+			testfail(new String[] { // test XON incorrect data
+				"aABB",
+				"\"\\A\"",
+				"\"\\u0xyz\"",
+				"[1,2,]",
+				"{\"\":1,}",
+				"[01.1]",
+				"[01]",
+				"[1.]",
+				"[.1]",
+				"d--01-",
+				"d11:20:31.123-020",
+				"P",
+				"PT+1H",
+				"PT+1H",
+			}, g, "xon");
 		} catch (Exception ex) {fail(ex);}
 	}
 
