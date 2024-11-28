a23c70865b5ea464bc196f714459a51adc1f4453
Vaclav Trojan <trojan@syntea.cz>
2024-11-28 18:40:00 +0100

    .

--- xdef/src/test/java/test/xdef/TestXComponents.java
+++ xdef/src/test/java/test/xdef/TestXComponents.java
@@ -824,9 +824,8 @@ public final class TestXComponents extends XDTester {
 		} catch (RuntimeException ex) {fail(ex);}
 ////////////////////////////////////////////////////////////////////////////////
 		try {// generate XCDPool from sources used in next tests
-			xp = compile(new String[] {
-				getDataDir()+"test/TestXComponents.xdef", getDataDir() + "test/TestXComponent_Z.xdef"}
-			);
+			xp = XDFactory.compileXD(null, new String[] {
+				getDataDir()+"test/TestXComponents.xdef", getDataDir() + "test/TestXComponent_Z.xdef"});
 			// generate and compile XComponents from xp
 			assertNoErrors(genXComponent(xp));
 		} catch (Exception ex) {
