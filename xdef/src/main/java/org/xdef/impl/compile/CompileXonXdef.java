c29768c3c0425f2c9b9490a1c760c48526663bdc
Václav Trojan <trojan@syntea.cz>
2025-10-26 13:39:35 +0100

    .

--- xdef/src/main/java/org/xdef/impl/compile/CompileXonXdef.java
+++ xdef/src/main/java/org/xdef/impl/compile/CompileXonXdef.java
@@ -375,29 +375,23 @@ public final class CompileXonXdef extends XScriptParser {
 						}
 					}
 				}
-			} else if (map.size() > 1) {
-				pn1 = genJElement(parent, X_MAP, map.getPosition());
-				if ("json".equals(parent.getLocalName())) { // map is root in the model
-					pn2 = pn1;
-				} else {
-					pn2 = genXDElement(pn1, "mixed", map.getPosition());
-					pn1.addChildNode(pn2);
-				}
-				if (!eos()) {
-					setXDAttr(pn1, "script", new SBuffer(getUnparsedBufferPart(), getPosition()));
-				}
-			} else {
+			} else if (map.size() <= 1 || "json".equals(parent.getLocalName())) {
+				//less then 2 items or parent is a json model => do not add "xd:mixed" node
 				pn2 = pn1 = genJElement(parent, X_MAP, map.getPosition());
-				if (!eos()) {
-					setXDAttr(pn1, "script", new SBuffer(getUnparsedBufferPart(), getPosition()));
-				}
+			} else { // more then 1 items -> // add "xd:mixed" element
+				pn1 = genJElement(parent, X_MAP, map.getPosition());
+				pn2 = genXDElement(pn1, "mixed", map.getPosition());
+				pn1.addChildNode(pn2);
 			}
-		} else if (map.size() > 1) {
+			if (!eos()) { // set xd:script attribute
+				setXDAttr(pn1, "script", new SBuffer(getUnparsedBufferPart(), getPosition()));
+			}
+		} else if (map.size() <= 1) { // less then 2 items
+			pn2 = pn1 = genJElement(parent, X_MAP, map.getPosition()); // do not add "xd:mixed" element
+		} else { // more then 1 items -> // add "xd:mixed" element
 			pn1 = genJElement(parent, X_MAP, map.getPosition());
 			pn2 = genXDElement(pn1, "mixed", map.getPosition());
 			pn1.addChildNode(pn2);
-		} else {
-			pn2 = pn1 = genJElement(parent, X_MAP, map.getPosition());
 		}
 		Object anyItem = null;
 		for (Map.Entry<Object, Object> entry: map.entrySet()) {
