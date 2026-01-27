579aac03ac464f20020ea031f46cb5828b587262
Vladimír Šišma <sisma@syntea.cz>
2026-01-27 17:16:15 +0100

    userdoc: vyvoj

--- xdef/src/userdoc/data-filter/examples/runAll.sh
+++ xdef/src/userdoc/data-filter/examples/runAll.sh
@@ -11,11 +11,14 @@ if  !  [ -e temp/test ]; then
    mkdir temp/test 
 fi
 
-echo Compile and run  ...
-javac -encoding UTF8 -classpath "temp/classes:../xdef.jar:lib/derby.jar:lib/saxon9-xqj.jar:lib/saxon9he.jar:lib/snakeyaml-1.9.jar" -d temp/classes src/data/MyClass.java src/GenDerby.java src/GenDerby.java src/RunAll_prepare.java src/data/*.java src/task6/GenComponents*.java
-java -classpath "temp/classes:../xdef.jar:lib/derby.jar:lib/saxon9-xqj.jar:lib/saxon9he.jar:lib/snakeyaml-1.9.jar" RunAll_prepare
-javac -encoding UTF8 -classpath "temp/classes:../xdef.jar:lib/derby.jar:lib/saxon9-xqj.jar:lib/saxon9he.jar:lib/snakeyaml-1.9.jar" -d temp/classes src/*.java src/task1/*.java src/task2/*.java src/task3/*.java src/task4/*.java src/task5/*.java src/task6/*.java  src/task6/components1/*.java src/task6/components2/*.java src/task7/*.java src/task8/*.java src/task9/*.java src/task10/*.java
-java -classpath "temp/classes:../xdef.jar:lib/derby.jar:lib/saxon9-xqj.jar:lib/saxon9he.jar:lib/snakeyaml-1.9.jar" RunAll
+cp="-classpath temp/classes:../xdef-${version}.jar:lib/derby-${derby.version}.jar:lib/SaxonHE-${saxon-he.version}-xqj.jar:lib/SaxonHE-${saxon-he.version}.jar:lib/snakeyaml-${snakeyaml.version}.jar"
+copts="${cp} -encoding UTF8 -d temp/classes"
+
+echo Compile and run ...
+javac ${copts} src/data/MyClass.java src/GenDerby.java src/GenDerby.java src/RunAll_prepare.java src/data/*.java src/task6/GenComponents*.java
+java ${cp} RunAll_prepare
+javac ${copts} src/*.java src/task1/*.java src/task2/*.java src/task3/*.java src/task4/*.java src/task5/*.java src/task6/*.java  src/task6/components1/*.java src/task6/components2/*.java src/task7/*.java src/task8/*.java src/task9/*.java src/task10/*.java
+java ${cp} RunAll
 
 if  [ -e derby.log ]; then
     rm derby.log
