bced01f16a5b1fc8454e17c833bdd7e49543cf25
Václav Trojan <trojan@syntea.cz>
2026-02-05 14:58:50 +0100

    .

---
+++ xdef/src/test/java/bugreports/subelem/PlatneOd1.java
@@ -0,0 +1,6 @@
+package bugreports.subelem;
+
+public interface PlatneOd1 extends org.xdef.component.XComponent {
+  public org.xdef.sys.SDatetime getPlatnostOd();
+  public void setPlatnostOd(org.xdef.sys.SDatetime x);
+}
