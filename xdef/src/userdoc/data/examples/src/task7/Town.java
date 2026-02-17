2bc968c037e170a936814ff8497e0faeb20461a6
Václav Trojan <trojan@syntea.cz>
2026-02-17 23:07:54 +0100

    .

--- xdef/src/userdoc/data/examples/src/task7/Town.java
+++ xdef/src/userdoc/data/examples/src/task7/Town.java
@@ -19,49 +19,46 @@ public class Town {
 		Element el;
 		ArrayReporter reporter = new ArrayReporter();
 
-		// 3. Create XDDOcument, set language, and process the localized version
-		// for the English language
+		// 3. Create XDDOcument, set language for the English language, and process the localized version
 		xd = xPool.createXDDocument("town");
 		xd.setLexiconLanguage("eng");
-		el = xd.xparse("task7/input/town_eng.xml", reporter);
+		xd.xparse("task7/input/town_eng.xml", reporter);
 		if (reporter.errors()) {
-			System.err.println("Error on English version");
+			System.err.println("Error on English version:\n" + reporter);
 		}
 
-		// 3. Create XDDOcument, set language, and process the localized version
-		// for the English language
+		// 3. Create XDDOcument, set language for the English language, and process the localized version
 		xd = xPool.createXDDocument("town");
 		xd.setLexiconLanguage("eng");
 		reporter.clear();
 		el = xd.xparse("task7/input/town_eng.xml", reporter);
 		if (reporter.errors()) {
-			System.err.println("Error on English version");
+			System.err.println("Error on English version:\n" + reporter);
 		} else {
 			KXmlUtils.writeXml("task7/output/town_eng.xml", el, true, false);
 			System.out.println("English version parsed");
 		}
 
-		// 4. Create XDDOcument, set language, and process the localized version
-		// for the German language
+		// 4. Create XDDOcument, set language for the German language, and process the localized version
 		xd = xPool.createXDDocument("town");
 		xd.setLexiconLanguage("deu");
 		reporter.clear();
 		el = xd.xparse("task7/input/town_deu.xml", reporter);
 		if (reporter.errors()) {
-			System.err.println("Error on German version");
+			System.err.println("Error on German version:\n" + reporter);
 		} else {
 			KXmlUtils.writeXml("task7/output/town_deu.xml", el, true, false);
 			System.out.println("town_eng translated to German (see task7/output/town_deu.xml))");
 		}
 
 		// 5. Create XDDOcument and translate the localized version to Czech.
-		// result write to the file "task7/output/town_ces.xml"
+		// The result write to "task7/output/town_ces.xml"
 		xd = xPool.createXDDocument("town");
 		reporter.clear();
 		el = xd.xtranslate("task7/input/town_deu.xml", "deu", "ces", reporter);
 		new File("task7/output").mkdirs();
 		if (reporter.errors()) {
-			System.err.println("Error on translation from 'deu' to 'ces'");
+			System.err.println("Error on translation from 'deu' to 'ces':\n" + reporter);
 		} else {
 			KXmlUtils.writeXml("task7/output/town_ces.xml", el, true, false);
 			System.out.println("town_deu translated to Czech (see task7/output/town_ces.xml)");
@@ -73,9 +70,8 @@ public class Town {
 		reporter.clear();
 		xd.xparse("task7/output/town_ces.xml", reporter);
 		if (reporter.errors()) {
-			System.err.println("task7.Town errors in translation");
-		} else {
-			System.out.println("OK, task7.Town");
+			System.err.println("Error in translation to Czech:\n" + reporter);
 		}
+		System.out.println("OK, task7.Town");
 	}
 }
