5acbcd0ab6fd36647c45434d2a40c112067845c4
Vladimír Šišma <sisma@syntea.cz>
2026-03-16 22:43:20 +0100

    xdef/userdoc: data-filter/examples: uprava skriptu - java-classpath, odstraneni adresare src/data/

--- xdef/src/userdoc/data-filter/examples/runAll.cmd
+++ xdef/src/userdoc/data-filter/examples/runAll.cmd
@@ -5,13 +5,13 @@ IF NOT EXIST temp\nul MD temp > nul
 IF NOT EXIST temp\classes\nul MD temp\classes > nul
 IF NOT EXIST temp\test\nul MD temp\test > nul
 
-set cp=-classpath temp/classes;../xdef-${project.version}.jar;lib/derby-${derby.version}.jar;lib/Saxon-HE-${saxon-he.version}-xqj.jar;lib/Saxon-HE-${saxon-he.version}.jar;lib/snakeyaml-${snakeyaml.version}.jar
+set cp=-classpath temp/classes;../xdef-${project.version}.jar;lib/*
 set copts=%cp% -encoding UTF8 -d temp/classes
 
 IF EXIST src\components\nul RD src\components /S /Q > nul
 javac %copts% src/RunAll_prepare.java src/task6/GenComponents*.java
 java %cp% RunAll_prepare > nul
-javac %copts% src/GenDerby.java src/*.java src/data/*.java src/task1/*.java src/task2/*.java src/task3/*.java src/task4/*.java src/task5/*.java src/task6/*.java src/task6/components1/*.java src/task6/components2/*.java src/task7/*.java src/task8/*.java src/task9/*.java src/task10/*.java
+javac %copts% src/*.java src/task1/*.java src/task2/*.java src/task3/*.java src/task4/*.java src/task5/*.java src/task6/*.java src/task6/components1/*.java src/task6/components2/*.java src/task7/*.java src/task8/*.java src/task9/*.java src/task10/*.java
 java %cp% RunAll
 
 IF EXIST derby.log DEL derby.log > nul
