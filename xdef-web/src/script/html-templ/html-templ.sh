a715da730f18216ad82a32a58977da33aff14039
Vladimír Šišma <sisma@syntea.cz>
2026-05-18 13:46:30 +0200

    web: html-transform: vyvoj

--- xdef-web/src/script/html-templ/html-templ.sh
+++ xdef-web/src/script/html-templ/html-templ.sh
@@ -3,13 +3,14 @@
 
 #konstanty
 saxonVersion=12.9
+xmlResVer=5.3.3
 #konstanty odvozene
 prgDir=$(dirname $(readlink -f $0))
 saxonPath=.m2/repository/net/sf/saxon/Saxon-HE/$saxonVersion/Saxon-HE-$saxonVersion.jar
 xslFile="$prgDir"/html-templ.xsl
 cp="${HOME}/${saxonPath}:\
-${HOME}/.m2/repository/org/xmlresolver/xmlresolver/5.3.3/xmlresolver-5.3.3.jar:\
-${HOME}/.m2/repository/org/xmlresolver/xmlresolver/5.3.3/xmlresolver-5.3.3-data.jar\
+${HOME}/.m2/repository/org/xmlresolver/xmlresolver/5.3.3/xmlresolver-${xmlResVer}.jar:\
+${HOME}/.m2/repository/org/xmlresolver/xmlresolver/5.3.3/xmlresolver-${xmlResVer}-data.jar\
 "
 
 #stahnout knihovnu Saxonu-HE pomoci "maven", pripadne rucne na:
@@ -17,15 +18,14 @@ ${HOME}/.m2/repository/org/xmlresolver/xmlresolver/5.3.3/xmlresolver-5.3.3-data.
 # - https://central.sonatype.com/artifact/org.xmlresolver/xmlresolver
 if [ ! -f $HOME/$saxonPath ]; then
     get="org.apache.maven.plugins:maven-dependency-plugin:3.10.0:get"
-    mvn $get -Dartifact="net.sf.saxon:Saxon-HE:$saxonVersion:jar"    || { exit 1; }
-    mvn $get -Dartifact="org.xmlresolver:xmlresolver:5.3.3:jar"      || { exit 1; }
-    mvn $get -Dartifact="org.xmlresolver:xmlresolver:5.3.3:jar:data" || { exit 1; }
-    exit
+    mvn $get -Dartifact="net.sf.saxon:Saxon-HE:$saxonVersion:jar"           || exit 1
+    mvn $get -Dartifact="org.xmlresolver:xmlresolver:${xmlResVer}:jar"      || exit 1
+    mvn $get -Dartifact="org.xmlresolver:xmlresolver:${xmlResVer}:jar:data" || exit 1
 fi
 
 
 
-#hlavni krok - vygenerovat dokumentaci z JIRA-xml-exportu tasku
+#hlavni krok - transformace webapp
 #for i in tutorial/ch??s??.html
 for i in tutorial/ch02s02.html
 do
