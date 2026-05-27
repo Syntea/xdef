ba6fa2cf17ed4b7c15a8a332ecb58cbceaefffe0
Václav Trojan <trojan@syntea.cz>
2026-05-27 10:33:05 +0200

    .

--- xdef/src/test/java/test/common/bnf/TestExpr.java
+++ xdef/src/test/java/test/common/bnf/TestExpr.java
@@ -140,7 +140,8 @@ if(true)return;
             assertNull(test("true", g, "print(!(true&false));"));
             assertNull(test("-1abc", g, "print( - ( ( 3 + 3 ) / 5 ) + \"abc\" );"));
             assertNull(test("1.26abc", g, "print(((3 + 3.3)/5) + 'abc');"));
-            assertNull(test("2", g, "print( - 1 + 3 );"));
+            assertNull(test("\n", g, "println();"));
+            assertNull(test("2\n", g, "println( - 1 + 3 );"));
             assertNull(test("1", g, " print(  min ( 2 , 1 ) ) ; "));
             assertNull(test("1.0", g, "print(min(2.0,1.0));"));
             assertNull(test("1.0",  g, "print(min(2,1.0));"));
@@ -326,7 +327,7 @@ if(true)return;
             assertNull(test("", g, "for (int i=0;i<4;i++)if(i==3)switch(i){case 2: print(i);break;} else break;"));
             assertNull(test("2", g,"for(int i=0; i < 4; i++) if(i==3)break; else switch(i){case 2: print(i);break;}"));
             assertNull(test("2", g,"for(int i=0;i<4;i++)if(i==3)continue; else switch(i){case 2: print(i);continue;}"));
-            assertNull(test("91",g, 
+            assertNull(test("91",g,
                 "for(int i = 0; i < 4; i++) if(i == 0) switch(i){case 0: print(9);break;} else {print(i); break;}"));
             assertNull(test("0122", g,
 "for(int i=0; i < 4; i++) {\n"+
