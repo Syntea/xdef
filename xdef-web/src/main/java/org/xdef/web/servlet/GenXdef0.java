18687b74e20acf5df59fb111aa40f9d0afa5a526
Vladimír Šišma <sisma@syntea.cz>
2026-08-08 12:28:49 +0200

    web: ulozeni puvodnich GenSchema0, GenXdef0

---
+++ xdef-web/src/main/java/org/xdef/web/servlet/GenXdef0.java
@@ -0,0 +1,106 @@
+package org.xdef.web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.w3c.dom.Element;
+import org.xdef.sys.Report;
+import org.xdef.sys.STester;
+import org.xdef.util.GenXDefinition;
+import org.xdef.web.util.ServletUtil;
+import org.xdef.xml.KXmlUtils;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/** Servlet for execution of examples from tutorial.
+ * @author Vaclav Trojan
+ */
+public final class GenXdef0 extends XdefServletAbs {
+
+    private static final long serialVersionUID = -815756752335589510L;
+
+    /** default constructor, calls super() only */
+    public GenXdef0() {
+        super();
+    }
+
+    /** Generate X-definition and run validation of given object with created X-definition.
+     * @param req servlet request object.
+     * @param resp servlet response object.
+     * @throws IOException if an error occurs.
+     */
+    @Override
+    public void processRequest(final HttpServletRequest req, final HttpServletResponse resp)
+        throws ServletException,IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        // This part we must synchronize to keep language settings for whole process of the X-definition.
+        synchronized(Report.class) {
+            Report.setLanguage("eng");
+            String data = ServletUtil.getParam(req, "data");
+            PrintWriter out = resp.getWriter();
+            try {
+                Element el = GenXDefinition.genXdef(data.trim(), "Example");
+                StringWriter swr = new StringWriter();
+                KXmlUtils.writeXml(swr, "UTF-8", el, " ", false, false, true);
+                swr.close();
+                String xdef = '\n' + swr.toString().trim() + '\n';
+                out.print(
+"<html xmlns='http://www.w3.org/1999/xhtml'>\n"+
+"  <head>\n"+
+"    <meta http-equiv='content-type' content='text/html; charset=UTF-8' />\n"+
+"    <script src='/tutorial/styles/lineNumbers.js'> </script>\n" +
+"    <script src='/tutorial/styles/lineNumbers_1.js'> </script>\n" +
+"    <style>.container{min-height:17rem;max-height:17rem;display:flex;overflow:hidden;}</style>\n" +
+"    <title>Validate data witn created X-definition</title>\n" +
+"  </head>\n" +
+"  <body style='background: #EAFFFD'>\n" +
+"    <form method='post' action='Playground'>\n" +
+"      <b>Created X-definition</b>\n" +
+"      <div class='container'>\n" +
+"        <div id='line-numbers' class='container_1'></div>\n" +
+"        <textarea id='textarea' style='width: 100%;' name='xdef'>\n" +
+ServletUtil.preTextToPreCont(xdef)+
+"</textarea>\n" +
+"      </div>\n"+
+"      <b>Input data</b>\n" +
+"      <div class=\"container\">\n" +
+"        <div id=\"line-numbers_1\" class=\"container_1\"></div>\n" +
+"        <textarea id=\"textarea_1\" style=\"width: 100%;\" name=\"data\">\n" +
+ServletUtil.preTextToPreCont(data).trim()+
+"</textarea>\n" +
+"      </div>\n");
+                if (data.startsWith("<")) { //data is XML format
+                    out.print(
+"      <input name='submit' value='Execute' type='submit' />\n");
+                } else {  // data is JSON format (??? - TODO other formats)
+                    out.print(
+"      <input type='hidden' name='dataFormat' value='xon'/>\n" +
+"      <input type='hidden' name='xonDisplayAs' value='json yaml xon xml'/>\n" +
+"      <button type='submit'>Execute</button>\n");
+                }
+                out.print(
+"      <i>&nbsp;\n" +
+"        You can edit <b>Created X-definition</b> or <b>Input</b> window\n" +
+"      </i>\n" +
+"    </form>\n" +
+"  </body>\n" +
+"</html>");
+            } catch (IOException | RuntimeException ex) {
+                out.print("<html><body><h1>Exception</h1>" +
+                    "<pre><tt><b>" + ServletUtil.preTextToPreCont(STester.printThrowable(ex))
+                    + "</b></tt></pre></body></html>");
+            }
+        }
+    }
+
+    /** Returns a short description of this servlet.
+     * @return short description of this servlet.     */
+    @Override
+    public final String getServletInfo() {return "This servlet creates an X-definition from given XML";}
+}
